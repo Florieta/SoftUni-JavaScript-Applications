a0d80731c848524517e243852b53b4091d97fbe9
Florieta <florieta.pencheva@gmail.com>
2022-03-29 16:02:03 +0300

    Added some problem solution of sample exam

--- Preparation Exam/Meme_Lounge/src/views/notifications.js
+++
@@ -1,9 +0,0 @@
-import { html } from "../helper.js";
-
-
-export const notifyTemplate = (err) => html`
-<section id="notifications">
-    <div id="errorBox" class="notification">
-        <span>${err}</span>
-    </div>
-</section>`
